d476643e2761322553203a5f402732504fb1f193
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2024-01-17 17:40:08 +0500

    created quiz model

---
+++ lib/src/feature/domain/models/quiz_model.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Quiz {
+  final int id;
+  final int difficulty;
+  final Widget image;
+
+  Quiz({required this.id, required this.difficulty, required this.image});
+}
