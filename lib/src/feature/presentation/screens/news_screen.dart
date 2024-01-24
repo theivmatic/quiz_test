b07a4ecebd43c697502b492c713306301fc3ec3d
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2024-01-24 14:11:15 +0500

    added appbar to  news screen

--- lib/src/feature/presentation/screens/news_screen.dart
+++ lib/src/feature/presentation/screens/news_screen.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class NewsScreen extends StatelessWidget {
   const NewsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Новости',
+          style: GoogleFonts.robotoFlex(
+            textStyle: const TextStyle(
+              fontSize: 19,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: const Color.fromRGBO(27, 31, 41, 1),
+      ),
+      backgroundColor: const Color.fromRGBO(27, 31, 41, 1),
+    );
   }
 }
