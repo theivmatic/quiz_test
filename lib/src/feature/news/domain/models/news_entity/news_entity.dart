aab50b548d40f6516e2f43e048d76103e41289c8
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2024-02-07 23:51:15 +0500

    created news model

---
+++ lib/src/feature/news/domain/models/news_entity/news_entity.dart
@@ -0,0 +1,31 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'news_entity.g.dart';
+
+@JsonSerializable()
+class NewsEntity {
+  String? title;
+  List<News>? news;
+
+  NewsEntity({this.title, this.news});
+
+  factory NewsEntity.fromJson(Map<String, dynamic> json) =>
+      _$NewsEntityFromJson(json);
+}
+
+@JsonSerializable()
+class News {
+  @JsonKey(name: 'news_title')
+  String? newsTitle;
+  @JsonKey(name: 'news_text')
+  String? newsText;
+  @JsonKey(name: 'image_path')
+  String? imagePath;
+  @JsonKey(name: 'news_subtext')
+  String? newsSubtext;
+
+  News({this.newsTitle, this.newsText, this.imagePath, this.newsSubtext});
+
+  factory News.fromJson(Map<String, dynamic> json) =>
+      _$NewsFromJson(json);
+}
