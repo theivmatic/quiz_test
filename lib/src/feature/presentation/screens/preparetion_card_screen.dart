d1a96b49c2cd44a7a10803ba67b51f53edfdab50
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2024-02-07 21:53:48 +0500

    finished preparation card screen

--- lib/src/feature/presentation/screens/preparetion_card_screen.dart
+++ lib/src/feature/presentation/screens/preparetion_card_screen.dart
@@ -49,12 +49,12 @@ class _PreparationCardScreenState extends State<PreparationCardScreen> {
         bloc: preparationBloc,
         builder: (context, state) => switch (state) {
           PreparationBlocLoadedState() => ListView.builder(
-              itemCount: state.preparationLoaded.facts!.length,
+              itemCount: state.preparationLoaded.facts?.length,
               itemBuilder: (context, index) => PreparationTileWidget(
-                number: state.preparationLoaded.facts![index].number!,
-                text: state.preparationLoaded.facts![index].text!,
-                imagePath: state.preparationLoaded.facts![index].imagePath!,
-                subtext: state.preparationLoaded.facts![index].subtext!,
+                number: state.preparationLoaded.facts?[index].number,
+                text: state.preparationLoaded.facts?[index].text,
+                imagePath: state.preparationLoaded.facts?[index].imagePath,
+                subtext: state.preparationLoaded.facts?[index].subtext,
               ),
             ),
           PreparationBlocLoadingState() => const Placeholder(),
@@ -67,8 +67,8 @@ class _PreparationCardScreenState extends State<PreparationCardScreen> {
 }
 
 class PreparationTileWidget extends StatelessWidget {
-  final String number;
-  final String text;
+  final String? number;
+  final String? text;
   final String? imagePath;
   final String? subtext;
 
@@ -82,13 +82,51 @@ class PreparationTileWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(number),
-        Text(text),
-        Image.asset(imagePath!),
-        Text(subtext!),
-      ],
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 10),
+          Text(
+            number ?? '',
+            style: GoogleFonts.robotoFlex(
+              textStyle: const TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.w500,
+                color: Color.fromRGBO(247, 247, 251, 1),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            text ?? '',
+            style: GoogleFonts.robotoFlex(
+              textStyle: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+                color: Color.fromRGBO(189, 193, 203, 1),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          imagePath != null ? Image.asset(imagePath!) : const SizedBox(),
+          const SizedBox(height: 5),
+          subtext != null
+              ? Text(
+                  subtext!,
+                  style: GoogleFonts.robotoFlex(
+                    textStyle: const TextStyle(
+                      fontSize: 11,
+                      fontWeight: FontWeight.w400,
+                      color: Color.fromRGBO(189, 193, 203, 1),
+                    ),
+                  ),
+                )
+              : const SizedBox(),
+          
+        ],
+      ),
     );
   }
 }
