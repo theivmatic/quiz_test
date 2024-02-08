aa35dfd91bf789dda805d672aa16265d78d198d7
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2024-02-09 00:05:41 +0500

    changed quiz json file and model

--- lib/src/feature/quiz/domain/models/quiz_entity.dart
+++ lib/src/feature/quiz/domain/models/quiz_entity.dart
@@ -7,51 +7,33 @@ part 'quiz_entity.g.dart';
 QuizEntity quizEntityFromJson(String jsonString) =>
     QuizEntity.fromJson(jsonDecode(jsonString) as Map<String, dynamic>);
 
-@JsonSerializable()
+@JsonSerializable() 
 class QuizEntity {
-  List<Question>? questions;
+	List<Question>? questions;
 
-  QuizEntity({
-    this.questions,
-  });
+	QuizEntity({this.questions});
 
-  factory QuizEntity.fromJson(Map<String, dynamic> json) =>
-      _$QuizEntityFromJson(json);
+  factory QuizEntity.fromJson(Map<String, dynamic> json) => _$QuizEntityFromJson(json);
 }
 
-@JsonSerializable()
+@JsonSerializable() 
 class Question {
-  int? id;
-  String? question;
-  List<Answer>? answers;
+	String? question;
+	List<Answer>? answers;
 
-  Question({
-    this.id,
-    this.question,
-    this.answers,
-  });
+	Question({this.question, this.answers});
 
-  factory Question.fromJson(Map<String, dynamic> json) =>
-      _$QuestionFromJson(json);
+  factory Question.fromJson(Map<String, dynamic> json) => _$QuestionFromJson(json);
 }
 
-@JsonSerializable()
+@JsonSerializable() 
 class Answer {
-  @JsonKey(name: 'first_answer')
-  String? firstAnswer;
-  @JsonKey(name: 'second_answer')
-  String? secondAnswer;
-  @JsonKey(name: 'third_answer')
-  String? thirdAnswer;
-  @JsonKey(name: 'fourth_answer')
-  String? fourthAnswer;
+  @JsonKey(name: 'answer_text')
+  String? answerText;
+  @JsonKey(name: 'is_correct')
+  bool? isCorrect;
 
-  Answer({
-    this.firstAnswer,
-    this.secondAnswer,
-    this.thirdAnswer,
-    this.fourthAnswer,
-  });
+	Answer({this.answerText, this.isCorrect});
 
   factory Answer.fromJson(Map<String, dynamic> json) => _$AnswerFromJson(json);
 }
--- lib/src/feature/quiz/domain/models/quiz_entity.g.dart
+++ lib/src/feature/quiz/domain/models/quiz_entity.g.dart
@@ -18,7 +18,6 @@ Map<String, dynamic> _$QuizEntityToJson(QuizEntity instance) =>
     };
 
 Question _$QuestionFromJson(Map<String, dynamic> json) => Question(
-      id: json['id'] as int?,
       question: json['question'] as String?,
       answers: (json['answers'] as List<dynamic>?)
           ?.map((e) => Answer.fromJson(e as Map<String, dynamic>))
@@ -26,21 +25,16 @@ Question _$QuestionFromJson(Map<String, dynamic> json) => Question(
     );
 
 Map<String, dynamic> _$QuestionToJson(Question instance) => <String, dynamic>{
-      'id': instance.id,
       'question': instance.question,
       'answers': instance.answers,
     };
 
 Answer _$AnswerFromJson(Map<String, dynamic> json) => Answer(
-      firstAnswer: json['first_answer'] as String?,
-      secondAnswer: json['second_answer'] as String?,
-      thirdAnswer: json['third_answer'] as String?,
-      fourthAnswer: json['fourth_answer'] as String?,
+      answerText: json['answer_text'] as String?,
+      isCorrect: json['is_correct'] as bool?,
     );
 
 Map<String, dynamic> _$AnswerToJson(Answer instance) => <String, dynamic>{
-      'first_answer': instance.firstAnswer,
-      'second_answer': instance.secondAnswer,
-      'third_answer': instance.thirdAnswer,
-      'fourth_answer': instance.fourthAnswer,
+      'answer_text': instance.answerText,
+      'is_correct': instance.isCorrect,
     };
