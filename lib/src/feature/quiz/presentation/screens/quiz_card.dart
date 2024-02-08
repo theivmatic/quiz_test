4c157769120e884ae7aa54464af0f6d0b9f7d457
Александр <alexandrfuss@Air-Aleksandr.mshome.net>
2024-02-08 18:09:35 +0500

    created quiz card screen

---
+++ lib/src/feature/quiz/presentation/screens/quiz_card.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_test/src/core/constants/app_theme.dart';
+
+class QuizCardScreen extends StatefulWidget {
+  const QuizCardScreen({super.key});
+
+  @override
+  State<QuizCardScreen> createState() => _QuizCardScreenState();
+}
+
+class _QuizCardScreenState extends State<QuizCardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Подготовка',
+          style: TextStyles.appBarText,
+        ),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: AppColors.darkBackground,
+      ),
+      backgroundColor: AppColors.darkBackground,
+    );
+  }
+}
--- lib/src/feature/quiz/presentation/screens/quiz_theme_screen.dart
+++ lib/src/feature/quiz/presentation/screens/quiz_theme_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_test/src/core/constants/app_theme.dart';
 import 'package:quiz_test/src/core/widgets/bottom_navigation_bar.dart';
+import 'package:quiz_test/src/feature/quiz/presentation/screens/quiz_card.dart';
 
 class QuizThemeScreen extends StatelessWidget {
   const QuizThemeScreen({super.key});
@@ -18,9 +19,25 @@ class QuizThemeScreen extends StatelessWidget {
         backgroundColor: AppColors.darkBackground,
       ),
       backgroundColor: AppColors.darkBackground,
-      body: const Placeholder(),
+      body: Column(
+        children: [
+          Center(
+            child: GestureDetector(
+              child: Image.asset(
+                'assets/images/quiz_topc.png',
+              ),
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute<dynamic>(
+                    builder: (context) => const QuizCardScreen(),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
       bottomNavigationBar: const BottomNavigationBarWidget(),
     );
-    
   }
 }
