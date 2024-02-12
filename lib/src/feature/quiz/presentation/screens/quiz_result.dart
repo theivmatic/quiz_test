3a5f744e0415439760fdd8e75dc0ac81e53a1680
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2024-02-12 22:06:22 +0500

    added dependency of result text to quiz result

--- lib/src/feature/quiz/presentation/screens/quiz_result.dart
+++ lib/src/feature/quiz/presentation/screens/quiz_result.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:quiz_test/src/core/constants/app_theme.dart';
 import 'package:quiz_test/src/core/router/navigation_screen.dart';
 import 'package:quiz_test/src/core/widgets/bottom_button.dart';
-import 'package:quiz_test/src/feature/quiz/presentation/screens/quiz_theme_screen.dart';
 
 class QuizResultScreen extends StatelessWidget {
   final int numberOfQuestions;
   final int rightAnswers;
 
-  const QuizResultScreen({super.key, required this.numberOfQuestions, required this.rightAnswers});
+  const QuizResultScreen({
+    super.key,
+    required this.numberOfQuestions,
+    required this.rightAnswers,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +35,9 @@ class QuizResultScreen extends StatelessWidget {
               height: 15,
             ),
             Text(
-              'Неплохо!\nНо вы можете лучше!',
+              rightAnswers == numberOfQuestions
+                  ? 'Вот это да!\nВы лучший из лучших!'
+                  : 'Неплохо!\nНо вы можете лучше!',
               style: TextStyles.factNumber,
               textAlign: TextAlign.center,
             ),
