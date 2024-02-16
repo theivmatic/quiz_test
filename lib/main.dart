85809b135f21d4739acd3f9ac3918c39ac0a9fa1
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2024-02-16 15:03:07 +0500

    wrapped MainApp in DevicePreview

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quiz_test/src/core/bloc/observer.dart';
@@ -12,7 +13,11 @@ import 'package:quiz_test/src/feature/quiz/domain/bloc/quiz_bloc.dart';
 void main() {
   Bloc.observer = AppObserver();
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MainApp());
+  runApp(
+    DevicePreview(
+      builder: (context) => const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
