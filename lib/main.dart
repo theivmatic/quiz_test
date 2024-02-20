362c3ac13598ceefd8721da750c6d94dc4583d00
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2024-02-20 13:25:54 +0500

    put back splash and start screens

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:quiz_test/src/core/bloc/observer.dart';
-import 'package:quiz_test/src/core/router/navigation_screen.dart';
-// import 'package:quiz_test/src/core/screens/splash_screen.dart';
-// import 'package:quiz_test/src/core/screens/start_screen.dart';
+import 'package:quiz_test/src/core/screens/splash_screen.dart';
 import 'package:quiz_test/src/feature/news/domain/bloc/news_bloc.dart';
 import 'package:quiz_test/src/feature/notes/domain/bloc/notes_bloc.dart';
 import 'package:quiz_test/src/feature/preparation/domain/bloc/preparation_bloc.dart';
@@ -51,7 +49,7 @@ class MainApp extends StatelessWidget {
           // debugShowCheckedModeBanner: false,
           locale: DevicePreview.locale(context),
           builder: DevicePreview.appBuilder,
-          home: const NavigationScreen(),
+          home: const SplashScreen(),
         ),
       ),
     );
